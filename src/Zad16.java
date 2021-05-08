5e74b991af29341e753edd904128bf338c96c5d5
Mariusz Błaszczyk <mariusz.blaszczyk@tuta.io>
2021-05-08 18:08:06 +0200

    16

--- src/Zad16.java
+++ src/Zad16.java
@@ -1,3 +1,7 @@
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Zad16 {
 
     /*
@@ -7,4 +11,52 @@ public class Zad16 {
     – losowana. Wyznacz taką parę, której odchylenie standardowe jest najmniejsze
     oraz taką parę, której odchylenie standardowe jest największe. Nie stosujemy tablic.
      */
+
+    static double getNumberFromUser(double min, double max) {
+        Scanner scan = new Scanner(System.in);
+        double number = 0;
+        boolean error = true;
+        do {
+            try {
+                System.out.println("Give a number between " + min + " - " + max);
+                do {
+                    number = scan.nextDouble();
+                    if (min > number || max < number) {
+                        System.out.println("The number cannot be less than " + min + " and bigger than " + max +
+                                ", please provide a new number.");
+                    }
+                } while (!(min <= number && number <= max));
+                error = false;
+            } catch (InputMismatchException e) {
+                System.out.println("You must give a number!");
+                scan.nextLine();
+            }
+        } while (error);
+        return number;
+    }
+
+    static double generateNumber(double min, double max) {
+        if (min >= max) {
+            throw new IllegalArgumentException("Random range is not correct.");
+        }
+        return Math.random() * (max - min) + min;
+    }
+
+    static double standardDeviation(double userNumber, double drawNumber) {
+        double avg = (userNumber + drawNumber) / 2.0;
+        double a = Math.pow(userNumber - avg, 2);
+        double b = Math.pow(drawNumber - avg, 2);
+        double x = a + b;
+        return Math.sqrt(x / 2.0);
+    }
+
+    public static void main(String[] args) {
+
+        double minUser = 4.5;
+        double maxUser = 10.5;
+
+        double minDraw = 3.5;
+        double maxDraw = 12.5;
+        
+    }
 }
