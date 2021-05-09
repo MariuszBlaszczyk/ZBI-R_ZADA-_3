b0174dbc176ebfd7c256b8dfcdc5077b0fdb8a82
Mariusz Błaszczyk <mariusz.blaszczyk@tuta.io>
2021-05-09 19:47:00 +0200

    19

--- src/Zad19.java
+++ src/Zad19.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Zad19 {
     /*
     Napisz program, w którym losujesz kolejne liczby całkowite z przedziału
@@ -7,4 +9,18 @@ public class Zad19 {
     wylosowano liczbę różniącą się od pierwszej wylosowanej liczby o co najmniej
     500. Nie stosujemy tablic.
      */
+
+    static int generateNumber(int min, int max) {
+        if (min >= max) {
+            throw new IllegalArgumentException("Random range is not correct");
+        }
+        return new Random().nextInt(max - min + 1) + min;
+    }
+
+
+    public static void main(String[] args) {
+
+        int min = 1;
+        int max = 400;
+    }
 }
