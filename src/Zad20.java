b435678c63c0f74829b2d26d0e4d3d94329bf888
Mariusz Błaszczyk <mariusz.blaszczyk@tuta.io>
2021-05-10 15:27:27 +0200

    20

--- src/Zad20.java
+++ src/Zad20.java
@@ -1,3 +1,7 @@
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Zad20 {
 
     /*
@@ -8,4 +12,60 @@ public class Zad20 {
     dodatkowo być liczbą mniejszą od b. Następnie wyznacz liczbę, która wystąpiła
     najwięcej razy pośród dwudziestu otrzymanych liczb. Nie stosujemy tablic
      */
+
+    static int generateNumber(int min, int max) {
+        if (min > max) {
+            throw new IllegalArgumentException("Random range is not correct");
+        }
+        return new Random().nextInt(max - min + 1) + min;
+    }
+
+    static int getNumberFromUser() {
+        Scanner scan = new Scanner(System.in);
+        int number = 0;
+        boolean error = true;
+        do {
+            try {
+                number = scan.nextInt();
+                error = false;
+            } catch (InputMismatchException e) {
+                System.err.println("You must give a number");
+                scan.nextLine();
+            }
+        } while (error);
+        return number;
+    }
+
+    static boolean isPrime(int number) {
+        if (number < 2) {
+            return false;
+        }
+        if (number == 2 || number == 3) {
+            return true;
+        }
+        double sqrt = Math.sqrt(number);
+        for (int i = 2; i <= sqrt; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+
+
+    public static void main(String[] args) {
+
+        int number1, number2;
+
+        do {
+            number1 = getNumberFromUser();
+            number2 = getNumberFromUser();
+        } while (!(number1 < number2));
+
+
+    }
 }
